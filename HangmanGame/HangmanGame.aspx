f672131a4836db722c971142c70d8b657544857d
Dilek Kocatürk <88059253+dilekkocaturk@users.noreply.github.com>
2023-04-27 22:40:31 +0300

    Add files via upload

---
+++ HangmanGame/HangmanGame.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HangmanGame.aspx.cs" Inherits="HangmanGame.HangmanGame" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hangman Game</title>
+    <style>
+        body {
+            text-align: center;
+            background-color: beige;
+        }
+        .gifRight{
+            position: absolute;
+            top: 200px;
+            left: 1020px;
+        }
+        .gifLeft{
+            position: absolute;
+            top: 200px;
+            left: 20px;
+        }
+        .button {
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Hangman Game</h1>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>  
+            <asp:Button ID="btnPlayAgain" runat="server" Text="Play Again" OnClick="btnPlayAgain_Click" />          
+            <asp:Timer ID="tmrCountDown" runat="server" Interval="1000" OnTick="tmrCountDown_Tick" ></asp:Timer>
+            <asp:Label ID="lblReTime" runat="server" Text="Remaining Time(s):"></asp:Label>
+            <asp:Label ID="lblCountDown" runat="server"></asp:Label>
+        </ContentTemplate>
+        </asp:UpdatePanel>          
+        <h3>Guess the word before the hangman is completed!</h3>
+        <asp:UpdatePanel ID="UpdatePanel2" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>    
+        <div class="button">
+            <asp:Button ID="btnHint" runat="server" Text="Hint" OnClick="btnHint_Click"/>
+            <asp:Label ID="lblHint" runat="server" Text="" Visible="false" ></asp:Label>
+        </div>
+        <asp:Image ID="imgHangman" runat="server" CssClass="button"/>    
+        <div>
+            <asp:Label ID="lblWord" runat="server" Text=""></asp:Label>
+        </div>       
+        <div id="pnlButtons" runat="server">
+            <div class="button">
+            <asp:Button ID="btnA" runat="server" Text="A" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnB" runat="server" Text="B" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnC" runat="server" Text="C" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnC2" runat="server" Text="Ç" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnD" runat="server" Text="D" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnE" runat="server" Text="E" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnF" runat="server" Text="F" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnG" runat="server" Text="G" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnG2" runat="server" Text="Ğ" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnH" runat="server" Text="H" OnClick="btnGuess_Click" />
+            </div>
+            <div class="button">
+            <asp:Button ID="btnI" runat="server" Text="I" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnI2" runat="server" Text="İ" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnJ" runat="server" Text="J" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnK" runat="server" Text="K" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnL" runat="server" Text="L" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnM" runat="server" Text="M" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnN" runat="server" Text="N" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnO" runat="server" Text="O" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnO2" runat="server" Text="Ö" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnP" runat="server" Text="P" OnClick="btnGuess_Click" />
+            </div>
+            <div class="button">
+            <asp:Button ID="btnR" runat="server" Text="R" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnS" runat="server" Text="S" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnS2" runat="server" Text="Ş" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnT" runat="server" Text="T" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnU" runat="server" Text="U" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnU2" runat="server" Text="Ü" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnV" runat="server" Text="V" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnY" runat="server" Text="Y" OnClick="btnGuess_Click" />
+            <asp:Button ID="btnZ" runat="server" Text="Z" OnClick="btnGuess_Click" />
+            </div>
+        </div>
+        <div>
+            <asp:Label ID="lblResult" runat="server" Text="" Font-Bold="true"></asp:Label>
+        </div>     
+        <div>
+            <asp:Image ID="imgGifRight" runat="server" CssClass="gifRight" />
+        </div>
+        <div>
+            <asp:Image ID="imgGifLeft" runat="server" CssClass="gifLeft" />
+        </div>    
+        </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ HangmanGame/HangmanGame.aspx.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace HangmanGame
+{
+    public partial class HangmanGame : System.Web.UI.Page
+    {     
+        private string selectedWord;
+        private string selectedHint;
+        private int wrongGuesses;
+        private int count = 60;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRandomWord();
+                DrawHangman(wrongGuesses);
+
+                tmrCountDown.Enabled = true;
+                count = 60;
+                lblCountDown.Text = count.ToString();
+            }
+            else
+            {
+                // Restore variables from ViewState
+                selectedWord = ViewState["SelectedWord"].ToString();
+                selectedHint = ViewState["SelectedHint"].ToString();
+                wrongGuesses = (int)ViewState["WrongGuesses"];    
+            }
+        }   
+        private void LoadRandomWord()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(Server.MapPath("~/words.xml"));
+            XmlNodeList nodeList = xml.SelectNodes("//Words/Word");
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, nodeList.Count);
+            selectedWord = nodeList[randomIndex]["Name"].InnerText.ToUpper();
+            selectedHint = nodeList[randomIndex]["Hint"].InnerText;
+            lblHint.Text = selectedHint;
+            lblWord.Text = string.Join(" ", selectedWord.ToCharArray().Select(c => c == ' ' ? "\u00A0" : "_"));
+
+            lblHint.Visible = false;
+
+            // Save changes to ViewState
+            ViewState["SelectedWord"] = selectedWord;
+            ViewState["SelectedHint"] = selectedHint;
+            ViewState["WrongGuesses"] = wrongGuesses;
+        }    
+        protected void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            Session.Clear(); // Clear Session to select new word
+            Response.Redirect(Request.RawUrl); // Refresh the page
+        }
+        protected void btnHint_Click(object sender, EventArgs e)
+        {
+            lblHint.Visible = true;
+            btnHint.Visible = false;
+        }
+        private void DrawHangman(int wrongGuesses)
+        {
+            switch (wrongGuesses)
+            {
+                case 0:
+                    imgHangman.ImageUrl = "~/Resources/default.png";
+
+                    break;
+                case 1:
+                    imgHangman.ImageUrl = "~/Resources/head.png";
+                    break;
+                case 2:
+                    imgHangman.ImageUrl = "~/Resources/body.png";
+                    break;
+                case 3:
+                    imgHangman.ImageUrl = "~/Resources/leftArm.png";
+                    break;
+                case 4:
+                    imgHangman.ImageUrl = "~/Resources/rightArm.png";
+                    break;
+                case 5:
+                    imgHangman.ImageUrl = "~/Resources/leftLeg.png";
+                    break;
+                case 6:
+                    imgHangman.ImageUrl = "~/Resources/rightLeg.png";
+                    break;
+                default:
+                    break;
+            }
+        }
+        protected void btnGuess_Click(object sender, EventArgs e)
+        {
+            string letter = ((Button)sender).Text.ToUpper();
+            if (selectedWord.Contains(letter))
+            {
+                // Convert word to char array by removing "_" symbols in lblWord
+                char[] word = lblWord.Text.Replace(" ", "").ToCharArray(); 
+
+                for (int i = 0; i < selectedWord.Length; i++)
+                {
+                    if (selectedWord[i] == letter[0])
+                    {
+                        // Add the correctly guessed letter to the corresponding position of the word
+                        word[i] = letter[0]; 
+
+                        lblResult.Text = "Right Guess";
+                        ((Button)sender).BackColor = System.Drawing.Color.LightGreen;
+                    }
+                }
+                // Write the word to the screen by re-inserting the "_" symbols
+                lblWord.Text = string.Join(" ", word.Select(c => c == ' ' ? "\u00A0" : c.ToString()));
+
+                // If the word no longer contains any "_" symbols, the game is won
+                if (!lblWord.Text.Contains("_")) 
+                {
+                    lblResult.Text = "Congratulations, you won!";
+                    btnHint.Enabled = false;
+                    PlayWinSound();
+                    imgGifRight.ImageUrl = "~/Resources/win.gif";
+                    imgGifRight.Visible = true;
+                    imgGifLeft.ImageUrl = "~/Resources/win.gif";
+                    imgGifLeft.Visible = true;
+                    tmrCountDown.Enabled = false;
+                    DisableButtons();    
+                }
+            }
+            else
+            {
+                lblResult.Text = "Wrong Guess";
+                ((Button)sender).BackColor = System.Drawing.Color.IndianRed;
+                wrongGuesses++;
+                ViewState["WrongGuesses"] = wrongGuesses;
+                DrawHangman(wrongGuesses);
+                if (wrongGuesses >= 6)
+                {
+                    lblResult.Text = $"Sorry, you lost! The word was {selectedWord}.";
+                    btnHint.Enabled = false;
+                    PlayLoseSound();
+                    imgGifRight.ImageUrl = "~/Resources/lose.gif";
+                    imgGifRight.Visible = true;
+                    imgGifLeft.ImageUrl = "~/Resources/lose.gif";
+                    imgGifLeft.Visible = true;
+                    tmrCountDown.Enabled = false;
+                    DisableButtons();
+
+                    lblWord.Text = selectedWord;      
+                }
+            }
+            ((Button)sender).Enabled = false;
+        }
+        protected void tmrCountDown_Tick(object sender, EventArgs e)
+        {
+            int countdown = int.Parse(lblCountDown.Text);
+
+            if (countdown > 0)
+            {
+                countdown--;
+                lblCountDown.Text = countdown.ToString();
+            }
+            else
+            {
+                UpdatePanel2.Update();
+                lblResult.Text = $"Time is up! The word was {selectedWord}.";
+                btnHint.Enabled = false;
+                PlayLoseSound();
+                imgGifRight.ImageUrl = "~/Resources/lose.gif";
+                imgGifRight.Visible = true;
+                imgGifLeft.ImageUrl = "~/Resources/lose.gif";
+                imgGifLeft.Visible = true;            
+                tmrCountDown.Enabled = false;
+                DisableButtons();
+
+                lblWord.Text = selectedWord;
+                DrawHangman(6);
+                
+            }
+        }
+        protected void PlayWinSound()
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = Server.MapPath("~/Resources/win.wav");
+            sp.Load();
+            sp.Play();
+        }
+        protected void PlayLoseSound()
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = Server.MapPath("~/Resources/lose.wav");
+            sp.Load();
+            sp.Play();
+        }
+        private void DisableButtons()
+        {
+            foreach (Control control in pnlButtons.Controls)
+            {
+                if (control is Button button)
+                    button.Enabled = false;
+            }
+        }
+        private void EnableButtons()
+        {
+            foreach (Control control in pnlButtons.Controls)
+            {
+                if (control is Button button)
+                    button.Enabled = true;
+            }
+        }
+    }
+}
