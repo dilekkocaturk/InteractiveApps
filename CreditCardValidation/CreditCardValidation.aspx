397d546bb824533aa888408531c96fec49f91a3b
Dilek Kocatürk <88059253+dilekkocaturk@users.noreply.github.com>
2023-04-27 22:39:57 +0300

    Add files via upload

---
+++ CreditCardValidation/CreditCardValidation.aspx
@@ -0,0 +1,114 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreditCardValidation.aspx.cs" Inherits="CreditCardValidation.CreditCardValidation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Credit Card Validation</title>
+    <style>
+        body {
+            background-image: url("https://www.paymentsjournal.com/wp-content/uploads/2017/05/Fotolia_90732811_Subscription_Monthly_M.jpg");
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+            text-align: center;
+        }
+        .card-icon {
+            position: absolute;
+            top: 350px;
+            left: 830px;
+        }
+        .card-number {
+            position: absolute;
+            top: 448px;
+            left: 668px;
+            font-size: 17px;
+            font-weight: bold;
+            color: white;
+            text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
+        }
+        .name-on-card {
+            position: absolute;
+            top: 490px;
+            left: 668px;
+            font-size: 17px;
+            font-weight: bold;
+            color: white;
+            text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
+        }
+        .exp {
+            position: absolute;
+            top: 490px;
+            left: 830px;
+            font-size: 13px;
+            font-weight: bold;
+            color: white;
+            text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
+        }
+    </style>
+</head>
+<body> 
+    <form id="form1" runat="server">
+        <div>     
+            <h1>Credit Card Information</h1>
+            <p>
+                <label for="txtNameOnCard">Name on Card:</label>
+                <asp:TextBox ID="txtNameOnCard" runat="server" onkeypress="var regex = /^[a-zA-Z\s]*$/; return regex.test(String.fromCharCode(event.charCode)) || event.keyCode == 13;"></asp:TextBox>
+            </p>
+            <p>
+                <label for="txtCardNumber">Card Number:</label>
+                <asp:TextBox ID="txtCardNumber" runat="server" onkeypress="return (event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 13"></asp:TextBox>
+            </p>
+            <p>
+                <label for="ddlExpirationMonth">Expiry Date (mm/yyyy):</label>
+                <asp:DropDownList ID="ddlExpirationMonth" runat="server">
+                    <asp:ListItem Value="01">01</asp:ListItem>
+                    <asp:ListItem Value="02">02</asp:ListItem>
+                    <asp:ListItem Value="03">03</asp:ListItem>
+                    <asp:ListItem Value="04">04</asp:ListItem>
+                    <asp:ListItem Value="05">05</asp:ListItem>
+                    <asp:ListItem Value="06">06</asp:ListItem>
+                    <asp:ListItem Value="07">07</asp:ListItem>
+                    <asp:ListItem Value="08">08</asp:ListItem>
+                    <asp:ListItem Value="09">09</asp:ListItem>
+                    <asp:ListItem Value="10">10</asp:ListItem>
+                    <asp:ListItem Value="11">11</asp:ListItem>
+                    <asp:ListItem Value="12">12</asp:ListItem>
+                </asp:DropDownList>
+                <asp:DropDownList ID="ddlExpirationYear" runat="server">
+                    <asp:ListItem Value="2022">2022</asp:ListItem>
+                    <asp:ListItem Value="2023">2023</asp:ListItem>
+                    <asp:ListItem Value="2024">2024</asp:ListItem>
+                    <asp:ListItem Value="2025">2025</asp:ListItem>
+                    <asp:ListItem Value="2026">2026</asp:ListItem>
+                    <asp:ListItem Value="2027">2027</asp:ListItem>
+                    <asp:ListItem Value="2028">2028</asp:ListItem>
+                    <asp:ListItem Value="2029">2029</asp:ListItem>
+                    <asp:ListItem Value="2030">2030</asp:ListItem>
+                    <asp:ListItem Value="2031">2031</asp:ListItem>
+                </asp:DropDownList>
+            </p>
+            <p>
+                <label for="txtCVV">CVV:</label>
+                <asp:TextBox ID="txtCVV" runat="server" onkeypress="return (event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 13"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+            </p>
+            <p>
+                <label for="lblResult"></label>
+                <asp:Label ID="lblResult" runat="server"></asp:Label>
+            </p>          
+            <div>
+            <asp:Image ID="imgCard" runat="server" ImageUrl="https://img.icons8.com/ios-filled/256/credit-card-front.png" />
+            </div>
+            <asp:Label ID="lblCardNumber" runat="server" CssClass="card-number"></asp:Label>
+            <asp:Label ID="lblNameOnCard" runat="server" CssClass="name-on-card"></asp:Label>
+            <asp:Label ID="lblExp" runat="server" CssClass="exp"></asp:Label>
+            <div>
+            <asp:Image ID="imgIcon" runat="server" CssClass="card-icon" Width="50" Height="50" Visible="false" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CreditCardValidation/CreditCardValidation.aspx.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CreditCardValidation
+{
+    public partial class CreditCardValidation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+            lblResult.Font.Bold = true;
+            lblCardNumber.Visible = false;
+            lblNameOnCard.Visible = false;
+            lblExp.Visible = false;
+            imgIcon.Visible = false;
+            lblResult.BackColor = System.Drawing.Color.White;
+
+            // Retrieve the input values from the form
+            string nameOnCard = txtNameOnCard.Text;
+            string cardNumber = txtCardNumber.Text;
+            string cvv = txtCVV.Text;
+            int expMonth = int.Parse(ddlExpirationMonth.SelectedValue);
+            int expYear = int.Parse(ddlExpirationYear.SelectedValue);
+
+            // Validate the credit card information
+            string cardType = IdentifyCardType(cardNumber);
+            bool isValid = ValidateCreditCard(cardNumber);
+            bool isCVVValid = ValidateCVV(cvv, cardType);
+            bool isExpirationValid = ValidateExpiration(expMonth, expYear);
+
+            // Display the validation result to the user
+            if (isValid && isCVVValid && isExpirationValid && !String.IsNullOrEmpty(nameOnCard))
+            {
+                lblNameOnCard.Text = nameOnCard;
+                lblNameOnCard.Visible = true;
+                lblExp.Text = expMonth.ToString() + "/" + (expYear % 100).ToString();
+                lblExp.Visible = true;
+
+                if (cardType == "Visa")
+                    imgIcon.ImageUrl = "https://cdn-icons-png.flaticon.com/512/349/349221.png";
+
+                else if (cardType == "MasterCard")
+                    imgIcon.ImageUrl = "https://icons-for-free.com/iconfiles/png/512/credit+card+mastercard+payment+shop+icon-1320167879935976478.png";
+
+                else if (cardType == "Discover")
+                    imgIcon.ImageUrl = "https://cdn-icons-png.flaticon.com/512/349/349230.png";
+
+                else if (cardType == "American Express")
+                    imgIcon.ImageUrl = "https://cdn-icons-png.flaticon.com/512/349/349228.png";
+
+                imgIcon.Visible = true;
+                lblResult.ForeColor = System.Drawing.Color.Green;
+                lblResult.Text = "Your credit card information is valid.";
+                imgCard.Visible = true;
+                lblCardNumber.Text = cardNumber;
+                lblCardNumber.Visible = true;
+            }
+            else
+            {
+                lblResult.ForeColor = System.Drawing.Color.Red;
+
+                if (String.IsNullOrEmpty(nameOnCard))
+                    lblResult.Text = "No name on card provided. ";
+
+                if (!isValid)
+                {
+                    if (String.IsNullOrEmpty(txtCardNumber.Text))
+                        lblResult.Text += "No card number provided. ";
+
+                    else if (!IsCardNumberLengthValid(txtCardNumber.Text))
+                        lblResult.Text += "Credit card number has an inappropriate number of digits. ";
+
+                    else if (!((IdentifyCardType(txtCardNumber.Text) == "Visa" || IdentifyCardType(txtCardNumber.Text) == "MasterCard" ||
+                                IdentifyCardType(txtCardNumber.Text) == "Discover" || IdentifyCardType(txtCardNumber.Text) == "American Express")))
+                        lblResult.Text += "Credit card number is in invalid format. ";
+
+                    else if (!ValidateLuhn(txtCardNumber.Text))
+                        lblResult.Text += "Credit card number is invalid. ";
+
+                    else
+                        lblResult.Text += "Unknown card type. ";
+                }
+
+                if (!isExpirationValid)
+                    lblResult.Text += "Expiration date is invalid. ";
+
+
+                if (!isCVVValid)
+                {
+                    if (String.IsNullOrEmpty(txtCVV.Text))
+                        lblResult.Text += "No CCV number provided. ";
+
+                    else
+                        lblResult.Text += "CCV number is invalid. ";
+                }
+            }
+        }
+
+        // To display the cardtype as an image of MasterCard, Visa etc.
+        private string IdentifyCardType(string cardNumber)
+        {
+            if (Regex.IsMatch(cardNumber, @"^4"))
+                return "Visa";
+
+            else if (Regex.IsMatch(cardNumber, @"^5[1-5]"))
+                return "MasterCard";
+            
+            else if (Regex.IsMatch(cardNumber, @"^6011"))
+                return "Discover";
+            
+            else if (Regex.IsMatch(cardNumber, @"^3[47]"))
+                return "American Express";
+            
+            else
+                return "";          
+        }
+        private bool IsCardNumberLengthValid(string cardNumber)
+        {
+
+            if (cardNumber.Length == 15 || cardNumber.Length == 16)
+                return true;
+            else
+                return false;
+        }
+        private bool ValidateCreditCard(string cardNumber)
+        {
+            // Check the prefix of the credit card number
+            //Visa
+            if (cardNumber.Length == 16 && cardNumber.StartsWith("4"))
+            {
+                return ValidateLuhn(cardNumber);
+            }
+            
+            //MasterCard
+            if (cardNumber.Length == 16 && cardNumber.StartsWith("5"))
+            {
+                int secondDigit = int.Parse(cardNumber[1].ToString());
+                if (secondDigit >= 1 && secondDigit <= 5)
+                    return ValidateLuhn(cardNumber);               
+            }
+            
+            //Discover
+            if (cardNumber.Length == 16 && cardNumber.StartsWith("6011"))
+                return ValidateLuhn(cardNumber);
+            
+            //American Express
+            if (cardNumber.Length == 15 && (cardNumber.StartsWith("34") || cardNumber.StartsWith("37")))
+                return ValidateLuhn(cardNumber);
+            
+            return false;
+        }
+        private bool ValidateLuhn(string cardNumber)
+        {
+            // Implement the Luhn algorithm to validate the credit card number
+            int sum = 0;
+            bool isAlternate = false;
+            for (int i = cardNumber.Length - 1; i >= 0; i--)
+            {
+                int currentDigit = int.Parse(cardNumber[i].ToString());
+
+                if (isAlternate)
+                {
+                    currentDigit *= 2;
+
+                    if (currentDigit > 9)
+                        currentDigit -= 9;
+                }
+
+                sum += currentDigit;
+                isAlternate = !isAlternate;
+            }
+
+            return sum % 10 == 0;
+        }
+        private bool ValidateCVV(string cvv, string cardType)
+        {
+            // Check the length of the CVV code
+            if (cardType == "American Express")
+                return cvv.Length == 4;
+
+            else
+                return cvv.Length == 3;          
+        }
+        private bool ValidateExpiration(int expMonth, int expYear)
+        {
+            // Check if the expiration date is in the future
+            DateTime expirationDate = new DateTime(expYear, expMonth, 1).AddMonths(1).AddDays(-1);
+            return expirationDate > DateTime.Now;
+        }
+    }
+}
